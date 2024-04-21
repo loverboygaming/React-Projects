cd8d45abc60aaca2fe921d7bdcacf1fa1d90aeb6
unknown <munikumarsvec143@gmail.com>
2024-04-21 14:56:33 +0530

    Updates

---
+++ precache-manifest.0d29f959380b99c6d393780cd16a426e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df02a664585699a857cdf6f991c19f33",
+    "url": "/React-Projects/index.html"
+  },
+  {
+    "revision": "e4dad04cdd3f2e4e48d4",
+    "url": "/React-Projects/static/css/main.541b88e7.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/React-Projects/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "e4dad04cdd3f2e4e48d4",
+    "url": "/React-Projects/static/js/main.a7e59e0b.chunk.js"
+  },
+  {
+    "revision": "d9111302033859240725",
+    "url": "/React-Projects/static/js/runtime~main.e756cf0a.js"
+  }
+]);
